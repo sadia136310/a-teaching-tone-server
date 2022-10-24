d5935ba4a8f3c577459da9b7da38ea1fac6d3cd5
sadia <sadiaislam1363@gmail.com>
2022-10-25 02:33:33 +0600

    fourth commit

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app =express();
 
 const cors =require('cors');
-app.use(cors());
-
 const Port =process.env.Port||5000;
 
+app.use(cors());
+const course=require('./data/data.json');
 
+app.get('/',(req,res)=>{
+res.send("Now server is running");
+});
+
+app.get('/course',(req,res)=>{
+    res.send(course);
+})
 
 app.listen(Port,()=>{
     console.log("server is running", Port);
